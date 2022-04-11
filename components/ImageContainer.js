d12a6bf873aa3997789021ed964e1506b67d6795
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-04-11 10:09:10 -0400

    Using next.js Image component

--- components/ImageContainer.js
+++ components/ImageContainer.js
@@ -1,11 +1,15 @@
+import Image from 'next/image';
+
 const ImageContainer = () => {
   return (
     <a href='#' className='group'>
       <div className='aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8'>
-        <img
+        <Image
           className='group-hover:opacity-75'
           src='https://bit.ly/placeholder-img'
           alt='Image'
+          layout='fill'
+          objectFit='cover'
         />
       </div>
       <h3 className='mt-4 text-sm text-gray-700'>Chris Davis</h3>
