dde13ec4ebe04b78df4b4c5d4b350e3e799daea1
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-04-11 10:16:09 -0400

    Added lazy loading blur effect

--- components/ImageContainer.js
+++ components/ImageContainer.js
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import Image from 'next/image';
 
 const ImageContainer = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <a href='#' className='group'>
       <div className='aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8'>
         <Image
-          className='group-hover:opacity-75'
+          className={`duration-700 ease-in-out group-hover:opacity-75 ${
+            isLoading
+              ? 'scale-110 blur-2xl grayscale'
+              : 'scale-100 blur-0 grayscale-0'
+          }`}
           src='https://bit.ly/placeholder-img'
           alt='Image'
           layout='fill'
           objectFit='cover'
+          onLoadingComplete={() => setIsLoading(false)}
         />
       </div>
       <h3 className='mt-4 text-sm text-gray-700'>Chris Davis</h3>
