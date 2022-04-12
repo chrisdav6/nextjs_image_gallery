144ccbc5c53534ec425a912b4ea3118ed336db74
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-04-12 09:12:21 -0400

    Troubleshooting API revalidate

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,14 +1,16 @@
-const handler = async (req, res) => {
+export default async function handler(req, res) {
+  // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
-    return res.status(401).json({ message: 'Invalid Token!' });
+    return res.status(401).json({ message: 'Invalid token' });
   }
 
   try {
+    // Regenerate our index route showing the images
     await res.unstable_revalidate('/');
     return res.json({ revalidated: true });
-  } catch (error) {
-    return res.status(500).send('Error Revalidating!');
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating');
   }
-};
-
-export default handler;
+}
