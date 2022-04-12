6435528bf86eea23108dfb2d7fac76eaa16ae425
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-04-12 08:58:01 -0400

    Add on-demand ISR

---
+++ pages/api/revalidate.js
@@ -0,0 +1,14 @@
+const handler = async (req, res) => {
+  if (req.query.secret !== ProcessingInstruction.env.REVALIDATE_SECRET) {
+    return res.status(401).json({ message: 'Invalid Token!' });
+  }
+
+  try {
+    await res.unstable_revalidate('/');
+    return res.json({ revalidated: true });
+  } catch (error) {
+    return res.status(500).send('Error Revalidating!');
+  }
+};
+
+export default handler;
